2919dcb0c0148e33c8365310cccd06c6ac8437a9
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-11-13 23:50:24 +0400

    port error fix try

--- server/index.js
+++ server/index.js
@@ -54,8 +54,9 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to database!");
-    server.listen(process.env.PORT, () => {
-      console.log("Server is running on port", process.env.PORT);
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
     });
   })
   .catch((err) => {
